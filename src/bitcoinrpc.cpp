fd7434d688784c098cd39696b720b79b5ec0e717
dkwzjw <beembanban@yahoo.co.jp>
2014-05-03 06:51:00 +0900

    fix rpcport

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -39,7 +39,7 @@ static boost::thread_group* rpc_worker_group = NULL;
 
 static inline unsigned short GetDefaultRPCPort()
 {
-    return GetBoolArg("-testnet", false) ? 18483 : 8483;
+    return GetBoolArg("-testnet", false) ? 18383 : 8383;
 }
 
 Object JSONRPCError(int code, const string& message)
